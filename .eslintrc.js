4265a6c6db952b5295ef7e015d19c6f778691486
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-14 22:42:18 -0300

    Configurando latitude e longitude inicial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+    'react-hooks',
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": ["error", { "endOfLine": "auto" }],
+    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+    "import/prefer-default-export": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-console": ["error", { allow: ["tron"]}],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
 };
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,18 +1,42 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
-import { View } from "react-native";
+import { View } from 'react-native';
 
 export default function Map() {
+  const [coordinates, setCoordinates] = useState({
+    latitude: -6.4793281,
+    longitude: -37.0722295,
+  });
+
+  useEffect(() => {
+    async function loadCurrentPosition() {
+      await Geolocation.getCurrentPosition(
+        ({ coords }) => {
+          setCoordinates(coords);
+        },
+        () => {},
+        {
+          timeout: 2000,
+          enableHighAccuracy: true,
+          maximumAge: 1000,
+        }
+      );
+    }
+
+    loadCurrentPosition();
+  }, []);
+
   return (
     <View style={{ flex: 1 }}>
       <MapView
         style={{ flex: 1 }}
-        region={{
-          latitude: -27.210753,
-          longitude: -49.644183,
+        initialRegion={{
+          latitude: coordinates.latitude,
+          longitude: coordinates.longitude,
           latitudeDelta: 0.0143,
-          longitudeDelta: 0.0134
+          longitudeDelta: 0.0134,
         }}
         showsUserLocation
         loadingEnabled
